879e74530c979dd0f44717fa1cb09acfce2b755e
Johnny Kaime <johnnyhou789@gmail.com>
2017-09-25 16:56:01 +0200

    using pointers - pointers for single linked list

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "prac2.h"
+
+int main(int argc, char * argv[]){
+	PrintList();
+	Length();
+	Push(&HEAD,'A');
+	Push(&HEAD,'B');
+	Push(&HEAD,'C');
+	PrintList();
+	Length();
+	Append(&HEAD,'Z');
+	Append(&HEAD,'Y');
+	PrintList();
+	Length();
+}
